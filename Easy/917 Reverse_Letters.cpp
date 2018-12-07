4e0a638e9e0cb87997c555af235e65cd29379b59
Mohit Gokul Murali <43658499+mohitgokul@users.noreply.github.com>
2018-12-06 17:41:01 -0800

    Create 917 Reverse_Letters.cpp

---
+++ Easy/917 Reverse_Letters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string S) {
+        int i = 0, j= S.length()-1;
+        while(i<j){
+            while (!isalpha(S[i])) i++;
+            while (!isalpha(S[j])) j--;
+            if(i>j) break;
+                swap(S[i],S[j]);
+            i++; 
+            j--;
+        }
+        return S;
+    }
+     
+};
