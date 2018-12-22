f9c9eb40af767e91d03112e14b53fdf04397478b
Mohit Gokul Murali <43658499+mohitgokul@users.noreply.github.com>
2018-12-22 07:30:32 -0600

    Create 53 Max_Subarray.js

---
+++ Easy/53 Max_Subarray.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let sum = nums[0];
+    let max = nums[0];
+    
+    for(let i = 1; i<nums.length; i++){
+        if(sum<0)
+            sum = nums[i];
+        else
+            sum += nums[i];
+        if(sum > max){
+            max = sum;
+        }
+    }
+    return max;
+};
